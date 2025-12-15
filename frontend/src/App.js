2c8474f112f1b83af93145d723084eeeaed7ae8c
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-12-16 03:29:36 +0530

    new login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
-import Authentication from './pages/authentication';
+import Login from './pages/login';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
 import HomeComponent from './pages/home';
@@ -10,24 +10,16 @@ import History from './pages/history';
 function App() {
   return (
     <div className="App">
-
       <Router>
-
         <AuthProvider>
-
-
           <Routes>
-
             <Route path='/' element={<LandingPage />} />
-
-            <Route path='/auth' element={<Authentication />} />
-
-            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/auth' element={<Login />} />
+            <Route path='/home' element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
-
       </Router>
     </div>
   );
